6bc58276ad314aefaacb20749bbbf4a25afa6d1c
noodle-strudel <yollaine369@gmail.com>
2023-01-05 22:45:12 -0800

    added double jump and test walls

--- Src/assassin.gd
+++ Src/assassin.gd
@@ -1,10 +1,13 @@
 extends KinematicBody2D
 
 export (int) var speed = 1200
-export (int) var jump_speed = -1800
+export (int) var jump_speed = -1000
 export (int) var gravity = 4000
+export (Vector2) var velocity = Vector2.ZERO
 
-var velocity = Vector2.ZERO
+
+# determines if double-jumping is possible 
+var double_jump = false
 
 func get_input():
 	velocity.x = 0
@@ -14,9 +17,20 @@ func get_input():
 		velocity.x -= speed
 
 func _physics_process(delta):
-	get_input()
-	velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
+	velocity.y += gravity * delta
+	on_floor(delta);
+
+
+func on_floor(delta):
+	get_input()
+	print("on wall: " , is_on_wall())
+	print("on floor: " , is_on_floor())
 	if Input.is_action_just_pressed("jump"):
 		if is_on_floor():
 			velocity.y = jump_speed
+			double_jump = true
+
+		elif double_jump == true:
+			velocity.y = jump_speed 
+			double_jump = false
