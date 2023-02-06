de1f33b0aa16a521a75155f06228ebb04f01be52
noodle-strudel <yollaine369@gmail.com>
2023-02-06 10:38:17 -0800

    made health and flipper items!

--- Src/GAME_SWITCHES.gd
+++ Src/GAME_SWITCHES.gd
@@ -4,7 +4,11 @@ enum {NORMAL, HIT, DED, ATTACK, REVIVE}
 var state
 var health
 var assassin_spawnpoint: Vector2
+
+var can_flip = false
 var gonna_flip = false
 var flipped = false
+
+
 func _ready():
 	pass
