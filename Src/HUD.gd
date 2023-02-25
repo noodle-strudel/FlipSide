37d7d0a18d789eb574c1ebfe953cde106d24336d
Secretly_cyborg <adeline.moseley42@gmail.com>
2023-02-24 17:29:48 -0800

    Trying to fix git T_T

--- Src/HUD.gd
+++ Src/HUD.gd
@@ -8,6 +8,8 @@ func _ready():
 func _process(delta):
 	$RichTextLabel.bbcode_text = "Heath: " + str(GameSwitches.health)
 
-
 func _on_Button_pressed():
 	emit_signal("respawn")
+
+func _on_Menu_pressed():
+	get_tree().change_scene("res://Scenes/menu.tscn")
