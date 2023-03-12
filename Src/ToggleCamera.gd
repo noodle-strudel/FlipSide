21b3b9753c30e2870cd6fdb147b94edeb4286ef4
noodle-strudel <yollaine369@gmail.com>
2023-03-12 16:32:33 -0700

    working on camera zooming

--- Src/ToggleCamera.gd
+++ Src/ToggleCamera.gd
@@ -5,8 +5,13 @@ onready var camera = get_node("/root/Level/Assassin/Camera2D")
 onready var tween = get_node("/root/Level/Assassin/Change Camera Zoom")
 
 func _on_ToggleCamera_body_entered(body):
-	zoom_camera(Vector2(1.5, 1.5), 2)
+	# Left
+	if body.global_position < global_position:
+		zoom_camera(Vector2(1.5, 1.5), 0.5)
+	# Right
+	elif body.global_position > global_position:
+		zoom_camera(Vector2(1.25, 1.25), 0.5)
 
-func zoom_camera(zoom: Vector2, time: int):
+func zoom_camera(zoom: Vector2, time: float):
 	tween.interpolate_property(camera, "zoom", camera.zoom, zoom, time, Tween.TRANS_LINEAR, Tween.EASE_OUT)
 	tween.start()
--- ToggleCameraRightHealth.gd
+++ ToggleCameraRightHealth.gd
@@ -2,5 +2,9 @@ extends ToggleCamera
 
 
 func _on_ToggleCameraRightHealth_body_entered(body):
+	# Left
 	if body.global_position < global_position:
-		zoom_camera(Vector2(1.25, 1.25), 1)
+		zoom_camera(Vector2(1.25, 1.25), 0.5)
+	# Right
+	elif body.global_position > global_position:
+		zoom_camera(Vector2(1.5, 1.5), 0.5)
