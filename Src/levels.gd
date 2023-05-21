20293bc250915069dafc51420e6ff2386a594184
noodle-strudel <yollaine369@gmail.com>
2023-05-20 19:44:42 -0700

    attempting to modularize levels forest and cave

---
+++ Src/levels.gd
@@ -0,0 +1,67 @@
+extends Node
+class_name level
+
+var dust_resource = preload("res://Scenes/dust.tscn")
+
+var flip_original = preload("res://Assets/Tileset/real_tileset.png")
+var flip_warp = preload("res://Assets/Tileset/flip tileset.png")
+
+export var music: Resource
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Assassin.position = GameSwitches.assassin_spawnpoint
+	Music.change_music(music)
+	
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), 0)
+
+func do_a_flip(bypass = false):
+	# Flips when they release the button
+	if Input.is_action_just_released("flip") || bypass == true:
+		$flipFlop.play()
+		get_tree().call_group("enemy", "flip")
+		if GameSwitches.flipped == false:
+			$"Details Foreground".tile_set.tile_set_texture(0, flip_warp)
+			$ParallaxBackground/Forest/ForestBackground.play("forestflip")
+			$ParallaxBackground/Cave/CaveBackground.play("caveflip")
+			GameSwitches.flipped = true
+		else:
+			$"Details Foreground".tile_set.tile_set_texture(0, flip_original)
+			$ParallaxBackground/Forest/ForestBackground.play("forestnoflip")
+			$ParallaxBackground/Cave/CaveBackground.play("cavenoflip")
+			GameSwitches.flipped = false
+		GameSwitches.gonna_flip = false
+
+func create_land_dust():
+	var dust = dust_resource.instance()
+	dust.position = $Assassin.position
+	dust.get_node("dust").animation = "landing"
+	add_child(dust)
+
+func die():
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), -5)
+	Music.change_music(Music.you_died)
+	$CanvasLayer/HUD/Retry.show()
+
+func create_air_dust():
+	var dust = dust_resource.instance()
+	dust.position = $Assassin.position
+	dust.get_node("dust").animation = "before_jump"
+	add_child(dust)
+
+func respawn():
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), 0)
+	$Assassin.position = GameSwitches.assassin_spawnpoint
+	GameSwitches.state = GameSwitches.REVIVE
+	GameSwitches.load_data()
+	
+	$Assassin.dead = false
+	$Assassin.reviving = true
+	
+	# collision layer for the assassin comes back after they are revived
+	
+	# "|" adds binary numbers together
+	$Assassin.collision_mask = GameSwitches.terrain_layer | GameSwitches.coin_layer
+	
+	$CanvasLayer/HUD/Retry.hide()
+	Music.change_music(music)
